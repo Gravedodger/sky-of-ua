d8c7ffbe253d52de0ba3178de027493d2ae646ef
Oleg Muraviov <o.muraviov2014@gmail.com>
2022-07-21 22:14:33 +0300

    social links
    added links

--- frontend/src/components/SocialLinks/SocialLinks.js
+++ frontend/src/components/SocialLinks/SocialLinks.js
@@ -10,22 +10,34 @@ const SocialLinks = (props) => {
   return (
     <ul className="socialLinks-list">
       <li className="socialLinks-item">
-        <a href="#" className="socialLinks-link">
+        <a
+          href="https://t.me/the_sky_of_ukraine"
+          className="socialLinks-link"
+          target="_blank"
+        >
           <IconTelegram className={`socialLinks-icon-${style}`} />
         </a>
       </li>
       <li className="socialLinks-item">
-        <a href="#" className="socialLinks-link">
+        <a
+          href="https://instagram.com/the_sky_of_ukraine?igshid=YmMyMTA2M2Y="
+          className="socialLinks-link"
+          target="_blank"
+        >
           <IconInstagram className={`socialLinks-icon-${style}`} />
         </a>
       </li>
       <li className="socialLinks-item">
-        <a href="#" className="socialLinks-link">
+        <a
+          href="https://www.facebook.com/groups/neboukrainu"
+          className="socialLinks-link"
+          target="_blank"
+        >
           <IconFacebook className={`socialLinks-icon-${style}`} />
         </a>
       </li>
     </ul>
-  )
-}
+  );
+};
 
 export default SocialLinks;

ebaf04bb04ca91b1d34e2bf9439cd75cfaa60365
Oleg Muraviov <o.muraviov2014@gmail.com>
2022-07-21 09:55:41 +0300

    fixed header nav menu
    fixed header nav menu

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -6,33 +6,30 @@ import NavMenu from "../NavMenu";
 import LanguageSwitch from "../LanguageSwitch";
 
 const Header = () => {
-    const [menuOn, setMenuOn] = useState(false);
-    const onClickBurger = () => {
-        setMenuOn((prevState) => !prevState);
-        document.querySelector(".header-burger").classList.toggle("active");
-    };
+  const [menuOn, setMenuOn] = useState(false);
+  const onClickBurger = () => {
+    console.log("clici");
+    setMenuOn((prevState) => !prevState);
+    document.querySelector(".header-burger").classList.toggle("active");
+  };
 
-    return (
-        <header>
-            <div className="main-container header-gradient">
-                <div className="header-wrap">
-                    <Logo style="header" />
-                    <NavMenu style="header" />
-                    <LanguageSwitch />
-
-                    <div className="header-burger" onClick={onClickBurger}>
-                        <span />
-                    </div>
-                    <MediaQuery maxWidth={767}>
-                        {menuOn && <NavMenu style="header" />}
-                    </MediaQuery>
-                    {/*<MediaQuery minWidth={768}>*/}
-                    {/*    {<NavMenu style="header" />}*/}
-                    {/*</MediaQuery>*/}
-                </div>
-            </div>
-        </header>
-    )
-}
+  return (
+    <header>
+      <div className="main-container header-gradient">
+        <div className="header-wrap">
+          <Logo style="header" />
+          <MediaQuery maxWidth={767}>
+            {menuOn && <NavMenu style="header" />}
+          </MediaQuery>
+          <MediaQuery minWidth={768}>{<NavMenu style="header" />}</MediaQuery>
+          <LanguageSwitch />
+          <div className="header-burger" onClick={onClickBurger}>
+            <span />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Header;

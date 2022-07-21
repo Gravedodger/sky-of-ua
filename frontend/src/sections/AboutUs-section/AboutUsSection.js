672cac6db5abd522fb02ac71eb8ba1692a603eb9
Oleg Muraviov <o.muraviov2014@gmail.com>
2022-07-21 14:42:47 +0300

    about section
    desktop styles

--- frontend/src/sections/AboutUs-section/AboutUsSection.js
+++ frontend/src/sections/AboutUs-section/AboutUsSection.js
@@ -1,38 +1,35 @@
-import React from 'react';
-import './AboutUsSection.css';
+import React from "react";
+import "./AboutUsSection.css";
 import AboutUsBlock from "../../blocks/AboutUs-block";
-import { SectionTitleBar, SectionTitleText } from "../../Typography";
+import { SectionTitleText } from "../../Typography";
 import { useTranslation } from "react-i18next";
 import "../../i18n";
 
 const AboutUsSection = () => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return (
-        <section>
-            <div className="section-title">
-                <SectionTitleBar />
-                <SectionTitleText>{t("about_us_section_title")}</SectionTitleText>
-            </div>
+  return (
+    <section className="about-us-section-wrap">
+      <div className="section-title">
+        <SectionTitleText>{t("about_us_section_title")}</SectionTitleText>
+      </div>
 
-            <div className="about-us-section-wrap">
-                <AboutUsBlock
-                    aboutUsBlockTitle={t("about_us_1.title")}
-                    aboutUsBlockText={t("about_us_1.text")}
-                />
-                <div className="vertical-line" />
-                <AboutUsBlock
-                    aboutUsBlockTitle={t("about_us_2.title")}
-                    aboutUsBlockText={t("about_us_2.text")}
-                />
-                <div className="vertical-line" />
-                <AboutUsBlock
-                    aboutUsBlockTitle={t("about_us_3.title")}
-                    aboutUsBlockText={t("about_us_3.text")}
-                />
-            </div>
-        </section>
-    )
-}
+      <ul className="about-us-content-wrap">
+        <AboutUsBlock
+          aboutUsBlockTitle={t("about_us_1.title")}
+          aboutUsBlockText={t("about_us_1.text")}
+        />
+        <AboutUsBlock
+          aboutUsBlockTitle={t("about_us_2.title")}
+          aboutUsBlockText={t("about_us_2.text")}
+        />
+        <AboutUsBlock
+          aboutUsBlockTitle={t("about_us_3.title")}
+          aboutUsBlockText={t("about_us_3.text")}
+        />
+      </ul>
+    </section>
+  );
+};
 
 export default AboutUsSection;
--- frontend/src/sections/OurTeam-section/OurTeamSection.js
+++ frontend/src/sections/OurTeam-section/OurTeamSection.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import './OurTeamSection.css';
-import { SectionTitleBar, SectionTitleText } from "../../Typography";
-import peopleHiking from '../../assets/images/people-hiking.png';
+import React from "react";
+import "./OurTeamSection.css";
+import { SectionTitleText } from "../../Typography";
+import peopleHiking from "../../assets/images/people-hiking.png";
 import { useTranslation } from "react-i18next";
-import '../../i18n';
+import "../../i18n";
 
 const OurTeamSection = () => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return (
-        <section className="our-team-section-wrap">
-            <div className="section-title">
-                <SectionTitleBar />
-                <SectionTitleText>{t("our_team_section.section_title")}</SectionTitleText>
-            </div>
+  return (
+    <section className="our-team-section-wrap">
+      <div className="section-title">
+        <SectionTitleText>
+          {t("our_team_section.section_title")}
+        </SectionTitleText>
+      </div>
 
-            <div className="our-team-section-container">
-                <div className="our-team-img">
-                    <img src={peopleHiking} alt={t("our_team_section.img_Alt")} />
-                </div>
-                <div className="our-team-description">
-                    <h4 className="our-team-description-title">
-                        {t("our_team_section.title")}
-                    </h4>
-                    <p className="our-team-description-text">
-                        {t("our_team_section.text")}
-                    </p>
-                </div>
-            </div>
-
-        </section>
-    )
-}
+      <div className="our-team-section-container">
+        <div className="our-team-img">
+          <img src={peopleHiking} alt={t("our_team_section.img_Alt")} />
+        </div>
+        <div className="our-team-description">
+          <h4 className="our-team-description-title">
+            {t("our_team_section.title")}
+          </h4>
+          <p className="our-team-description-text">
+            {t("our_team_section.text")}
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default OurTeamSection;

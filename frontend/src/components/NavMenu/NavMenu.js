a07ba5c8c93bc93918c8a8e7a0835e29714a964f
Oleg Muraviov <o.muraviov2014@gmail.com>
2022-07-21 21:08:06 +0300

    hide market place
    hide market place

--- frontend/src/components/NavMenu/NavMenu.js
+++ frontend/src/components/NavMenu/NavMenu.js
@@ -7,34 +7,42 @@ import { useTranslation } from "react-i18next";
 const NavMenu = (props) => {
   const { t } = useTranslation();
   const style = props.style;
-  const getActiveLink = ({ isActive }) => isActive ? `active nav-link-${style}` : `nav-link-${style}`;
+  const getActiveLink = ({ isActive }) =>
+    isActive ? `active nav-link-${style}` : `nav-link-${style}`;
 
   return (
     <nav className={`nav-overlay-${style}`}>
       <ul className={`nav-list-${style}`}>
         <li className={`nav-item-${style}`}>
           <NavLink to="/" className={getActiveLink}>
-            <NavText className={`nav-text-${style}`}>{t("nav_about_us")}</NavText>
+            <NavText className={`nav-text-${style}`}>
+              {t("nav_about_us")}
+            </NavText>
           </NavLink>
         </li>
         <li className={`nav-item-${style}`}>
           <NavLink to="/projects" className={getActiveLink}>
-            <NavText className={`nav-text-${style}`}>{t("nav_projects")}</NavText>
+            <NavText className={`nav-text-${style}`}>
+              {t("nav_projects")}
+            </NavText>
           </NavLink>
         </li>
         <li className={`nav-item-${style}`}>
           <NavLink to="/reports" className={getActiveLink}>
-            <NavText className={`nav-text-${style}`}>{t("nav_reports")}</NavText>
+            <NavText className={`nav-text-${style}`}>
+              {t("nav_reports")}
+            </NavText>
           </NavLink>
         </li>
-        <li className={`nav-item-${style}`}>
+        {/*  МАРКЕТ ПЛЕЙСТ ПОКИ НЕ ПОТРІБЕН */}
+        {/* <li className={`nav-item-${style}`}>
           <NavLink to="/marketplace" className={getActiveLink}>
             <NavText className={`nav-text-${style}`}>{t("nav_marketplace")}</NavText>
           </NavLink>
-        </li>
+        </li> */}
       </ul>
     </nav>
-  )
-}
+  );
+};
 
 export default NavMenu;

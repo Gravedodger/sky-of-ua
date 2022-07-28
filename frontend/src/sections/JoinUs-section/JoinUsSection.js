107122df87a532f4eed737254fa4db39196a66ce
Oleg Muraviov <o.muraviov2014@gmail.com>
2022-07-28 19:44:48 +0300

    join us adaptive

--- frontend/src/sections/JoinUs-section/JoinUsSection.js
+++ frontend/src/sections/JoinUs-section/JoinUsSection.js
@@ -1,51 +1,41 @@
-import React from 'react';
-import './JoinUsSection.css';
+import React from "react";
+import "./JoinUsSection.css";
 import { SectionTitleText } from "../../Typography";
 import { useTranslation } from "react-i18next";
-import '../../i18n';
+import "../../i18n";
 
 const JoinUsSection = () => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return (
-        <section className="join-us-section-wrap">
-            <div className="section-title">
-                <SectionTitleText>
-                    {t("join_us_section_title")}
-                </SectionTitleText>
+  return (
+    <section className="join-us-section-wrap">
+      <div className="section-title">
+        <SectionTitleText>{t("join_us_section_title")}</SectionTitleText>
+      </div>
+      <div className="join-us-section-content">
+        <div className="card-info-main">
+          <div className="card-description">
+            <h4 className="card-info-title">{t("larger_card.title")}</h4>
+            <p className="card-info-text">{t("larger_card.text")}</p>
+          </div>
+        </div>
+        <div className="card-info-add">
+          <div className="card-info-top">
+            <div className="card-description">
+              <h4 className="card-info-title">{t("top_card.title")}</h4>
+              <p className="card-info-text">{t("top_card.text")}</p>
             </div>
-
-            <div className="join-us-section-container">
-                <div className="join-us-section-container--left">
-                    <div className="card-item-one">
-                        <div className="card-info-one">
-                            <h4 className='card-info-title'>{t("larger_card.title")}</h4>
-                            <div className="card-divider" />
-                            <p className="card-description">{t("larger_card.text")}</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="join-us-section-container--right">
-                    <div className="card-item-two">
-                        <div className="card-info-two">
-                            <h4 className='card-info-title'>{t("top_card.title")}</h4>
-                            <div className="card-divider" />
-                            <p className="card-description">{t("top_card.text")}</p>
-                        </div>
-                    </div>
-
-                    <div className="card-item-three">
-                        <div className="card-info-two">
-                            <h4 className='card-info-title'>{t("bottom_card.title")}</h4>
-                            <div className="card-divider" />
-                            <p className="card-description">{t("bottom_card.text")}</p>
-                        </div>
-                    </div>
-                </div>
+          </div>
+          <div className="card-info-bottom">
+            <div className="card-description">
+              <h4 className="card-info-title">{t("bottom_card.title")}</h4>
+              <p className="card-info-text">{t("bottom_card.text")}</p>
             </div>
-        </section>
-    )
-}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default JoinUsSection;

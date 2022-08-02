7a63959247b90a0cec71a55db0d357b837cd4516
Oleg Muraviov <o.muraviov2014@gmail.com>
2022-08-02 14:37:34 +0300

    fix hero to only first page

--- frontend/src/sections/AboutUs-section/AboutUsSection.js
+++ frontend/src/sections/AboutUs-section/AboutUsSection.js
@@ -9,26 +9,28 @@ const AboutUsSection = () => {
   const { t } = useTranslation();
 
   return (
-    <section className="about-us-section-wrap">
-      <div className="section-title">
-        <SectionTitleText>{t("about_us_section_title")}</SectionTitleText>
-      </div>
+    <>
+      <section className="about-us-section-wrap">
+        <div className="section-title">
+          <SectionTitleText>{t("about_us_section_title")}</SectionTitleText>
+        </div>
 
-      <ul className="about-us-content-wrap">
-        <AboutUsBlock
-          aboutUsBlockTitle={t("about_us_1.title")}
-          aboutUsBlockText={t("about_us_1.text")}
-        />
-        <AboutUsBlock
-          aboutUsBlockTitle={t("about_us_2.title")}
-          aboutUsBlockText={t("about_us_2.text")}
-        />
-        <AboutUsBlock
-          aboutUsBlockTitle={t("about_us_3.title")}
-          aboutUsBlockText={t("about_us_3.text")}
-        />
-      </ul>
-    </section>
+        <ul className="about-us-content-wrap">
+          <AboutUsBlock
+            aboutUsBlockTitle={t("about_us_1.title")}
+            aboutUsBlockText={t("about_us_1.text")}
+          />
+          <AboutUsBlock
+            aboutUsBlockTitle={t("about_us_2.title")}
+            aboutUsBlockText={t("about_us_2.text")}
+          />
+          <AboutUsBlock
+            aboutUsBlockTitle={t("about_us_3.title")}
+            aboutUsBlockText={t("about_us_3.text")}
+          />
+        </ul>
+      </section>
+    </>
   );
 };
 

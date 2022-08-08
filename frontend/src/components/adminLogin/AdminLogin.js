acdb8d3126efceadbe1dff13529fac88e15716f6
Sviatoslav Malyshevskyi <s.malyshevskyi.08@gmail.com>
2022-08-09 00:06:56 +0300

    added tooltip for the Admin Login

--- frontend/src/components/adminLogin/AdminLogin.js
+++ frontend/src/components/adminLogin/AdminLogin.js
@@ -5,9 +5,17 @@ import UserIcn from './../../assets/icons/user.png';
 const AdminLogin = () => {
 	return (
 		<div className="admin-login-container">
-			<a href="http://localhost:1337/admin" target="_blank" rel="noopener noreferrer" >
+			<a href="http://localhost:1337/admin"
+			   target="_blank"
+			   rel="noopener noreferrer"
+			   className="tooltip"
+			>
 				<img src={UserIcn} className="admin-login-icon" alt="admin login" />
+				<span className="tooltip-text">
+					First run from the backend folder => yarn dev
+				</span>
 			</a>
+
 		</div>
 	)
 };

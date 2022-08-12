4be59f112032b0eada32e228a3f25a66e2b53a6f
Sviatoslav Malyshevskyi <s.malyshevskyi.08@gmail.com>
2022-08-13 02:50:01 +0300

    project updated as 13-08-2022

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,7 @@ import ProjectsInterface from "./components/ADMIN/Projects/ProjectsInterface";
 
 function App() {
   return (
-    <>
       <AppRoutes />
-      <ProjectsInterface />
-    </>
   )
 }
 

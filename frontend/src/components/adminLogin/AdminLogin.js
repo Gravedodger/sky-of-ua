1c34c2fd768e520812af48c879fd9e4ce25ffe71
Oleg Muraviov <o.muraviov2014@gmail.com>
2022-08-16 16:04:23 +0300

    adminLogin adaptive

--- frontend/src/components/adminLogin/AdminLogin.js
+++ frontend/src/components/adminLogin/AdminLogin.js
@@ -1,28 +1,29 @@
-import React from 'react';
-import './AdminLogin.css';
-import UserIcn from './../../assets/icons/user.png';
+import React from "react";
+import "./AdminLogin.css";
+import UserIcn from "./../../assets/icons/user.png";
 
-import  CreateIcon from '@mui/icons-material/Create';
+import CreateIcon from "@mui/icons-material/Create";
 
 const AdminLogin = () => {
-	return (
-		<div className="admin-login-container">
-			<a href="http://localhost:1337/admin"
-			   target="_blank"
-			   rel="noopener noreferrer"
-			   className="tooltip"
-			>
-				<img src={UserIcn} className="admin-login-icon" alt="admin login" />
-				<span className="tooltip-text">
-					First run from the backend folder => yarn dev
-				</span>
-			</a>
+  return (
+    <div className="admin-login-container">
+      <a
+        href="http://localhost:1337/admin"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="tooltip"
+      >
+        <img src={UserIcn} className="admin-login-icon" alt="admin login" />
+        <span className="tooltip-text">
+          First run from the backend folder ={">"} yarn dev
+        </span>
+      </a>
 
-			<a href="/ProjectsAdminPage" target="_blank">
-				<CreateIcon marginTop="10px" />
-			</a>
-		</div>
-	)
+      <a href="/ProjectsAdminPage" target="_blank">
+        <CreateIcon />
+      </a>
+    </div>
+  );
 };
 
 export default AdminLogin;

f3299835887e84d6396de397ac6d10fe4d079d08
Sviatoslav Malyshevskyi <s.malyshevskyi.08@gmail.com>
2022-08-20 18:10:10 +0300

    added routes and schemas to backend

---
+++ backend/schemas/project.js
@@ -0,0 +1,54 @@
+const { Schema } = require("mongoose");
+
+const projectSchema = new Schema ({
+	title: {
+		type: String,
+		required: true,
+		minLength: 5
+	},
+
+	description: {
+		type: String,
+		required: true,
+		minLength: 10
+	},
+
+	fullText: {
+	type: String,
+	required: true,
+	minLength: 20
+	},
+
+	image: {
+		type: Image,
+		required: true,
+	},
+
+	requiredAmount: {
+		type: Number,
+		required: true,
+		min: 1
+	},
+
+	collectedAmount: {
+		type: Number,
+		required: true,
+		max: 999999999
+	},
+
+	region: {
+		type: String,
+		required: true,
+		enum: ["Volyn region", "Rivne region", "Zhytomyr region", "Kyiv region", "Kyiv city",
+			"Chernihiv region", "Summy region", "Lviv region", "Ternopil region",
+			"Khmelnytskyy region", "Vinnytsya region", "Cherkasy region", "Poltava region",
+			"Kharkiv region", "Zakarpattya region", "Ivano-Frankivsk region", "Chernivtsy region",
+			"Kirovohrad region", "Dnipropetrovsk region", "Donetsk region", "Luhansk region",
+			"Odessa region", "Mykolayiv region", "Kherson region", "Zaporizhia region", "Autonomous Republic of Crimea",
+			"City of Sevastopil"
+		],
+		default: "Kyiv city"
+	}
+});
+
+module.exports = projectSchema;

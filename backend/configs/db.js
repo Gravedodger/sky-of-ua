543182a7666af4e605ddab4779902fde988ed4c2
Sviatoslav Malyshevskyi <s.malyshevskyi.08@gmail.com>
2022-08-28 01:05:18 +0300

    mongodb connection establ9ished

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-	mongoURI: 'mongodb+srv://Sviatoslav:dock157@sky-of-ua--test.ugkj5hh.mongodb.net/?retryWrites=true&w=majority',
+	mongoURI: 'mongodb://Sviatoslav:dock157@realm.mongodb.com:27020/?authMechanism=PLAIN&authSource=%24external&ssl=true&appName=data-uvxvx:sky-of-ua--test:local-userpass',
 	secretOrKey: 'secret',
 };
